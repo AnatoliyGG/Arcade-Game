2bb60d59adecded80a7d8844bf075b29d89726bb
Anatoliy <mazkovgg@gmail.com>
2022-12-07 12:47:27 +0300

    Bug fix, check OtherActor

--- Source/MyArcade/Actors/PlayGroundBorder.cpp
+++ Source/MyArcade/Actors/PlayGroundBorder.cpp
@@ -2,6 +2,7 @@
 
 #include "PlayGroundBorder.h"
 #include "Components/BoxComponent.h"
+#include "MyArcade/Pawns/MainPlayer.h"
 
 // Sets default values
 APlayGroundBorder::APlayGroundBorder()
@@ -15,6 +16,9 @@ void APlayGroundBorder::NotifyActorEndOverlap(AActor* OtherActor)
 {
 	Super::NotifyActorEndOverlap(OtherActor);
 	UE_LOG(LogTemp, Log, TEXT("OUT OF PLAYGROUND: %s"), *OtherActor->GetName());
+
+	if (!OtherActor) return;
+	if (Cast<AMainPlayer>(OtherActor)) return;
 
 	OtherActor->Destroy();
 }
