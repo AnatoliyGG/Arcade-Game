36462747a5611e254e9ed0b44bdbcc8ebb02189d
Anatoliy <mazkovgg@gmail.com>
2022-12-15 13:51:22 +0300

    Create new class BonusShootLevelUp

---
+++ Source/MyArcade/Actors/Bonuses/BonusShootLevelUp.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Bonus.h"
+#include "BonusShootLevelUp.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYARCADE_API ABonusShootLevelUp : public ABonus
+{
+	GENERATED_BODY()
+	
+};
